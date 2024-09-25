3fc1139b66f0ab212b61555e4614a2e14495674c
Wooseong Kim <carpe@29cm.co.kr>
2024-09-25 19:16:20 +0900

    restore/index.ts 를 macOS 기준으로 작성

--- src/restore/index.ts
+++ src/restore/index.ts
@@ -1,30 +1,30 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as p from 'path'
 import {
   checkKey,
   checkPaths,
-  exec,
   getCacheBase,
   getCachePath
 } from '../utils/cache'
 
 async function run(): Promise<void> {
   try {
-    /* 
-      clean up caches
-    */
-      const cacheBase = core.getState('cache-base')
-      const cleanKey = core.getInput('clean-key')
-      const CLEAN_TIME = 7
-  
-      if (cleanKey) {
-        await exec(
-          `/bin/bash -c "find ${cacheBase} -maxdepth 1 -name '${cleanKey}*' -type d -atime +${CLEAN_TIME} -exec rm -rf {} +"`
-        )
-      }
-    } catch (error) {
-      if (error instanceof Error) core.warning(error.message)
+    const cacheBase = core.getState('cache-base')
+    const cleanKey = core.getInput('clean-key')
+    const CLEAN_TIME = 7
+
+    if (cleanKey) {
+      const findCommand = `/bin/bash -c "find ${cacheBase} -depth 1 -name '${cleanKey}*' -type d -atime +${CLEAN_TIME} -exec rm -rf {} +"`
+      core.debug(`Executing find command: ${findCommand}`)
+      const findResult = await exec.getExecOutput(findCommand)
+
+      core.debug(findResult.stdout)
+      if (findResult.stderr) core.error(findResult.stderr)
     }
+  } catch (error) {
+    if (error instanceof Error) core.warning(error.message)
+  }
 
   try {
     const key = core.getInput('key')
@@ -43,9 +43,9 @@ async function run(): Promise<void> {
     core.saveState('cache-base', cacheBase)
     core.saveState('cache-path', cachePath)
 
-    await exec(`mkdir -p ${cacheBase}`)
-    const find = await exec(
-      `find ${cacheBase} -maxdepth 1 -name ${key} -type d`
+    await exec.exec(`mkdir -p ${cacheBase}`)
+    const find = await exec.getExecOutput(
+      `find ${cacheBase} -depth 1 -name ${key} -type d`
     )
     const cacheHit = find.stdout ? true : false
     core.saveState('cache-hit', String(cacheHit))
@@ -60,7 +60,7 @@ async function run(): Promise<void> {
         // 심볼릭 링크만 생성
         command = `ln -s ${p.join(cachePath, path.split('/').slice(-1)[0])} ./${path}`
       }
-      const result = await exec(command)
+      const result = await exec.getExecOutput(command)
 
       core.info(result.stdout)
       if (result.stderr) core.error(result.stderr)
